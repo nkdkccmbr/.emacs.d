fe03d8b8719bedb7a07087373df491518648831d
nkdkccmbr <nkdkccmbr@users.noreply.github.com>
2019-06-20 13:05:35 +0900

    Add files via upload

--- init.el
+++ init.el
@@ -1,12 +1,4 @@
-;; カスタマイズ用のファイルを設定
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-
+(add-to-list 'load-path "~/.emacs.d")
 (setq custom-file "./custom_settings.el")
- 
-;; カスタマイズ用ファイルをロード
 (load custom-file t)
