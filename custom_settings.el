5aa2645d30d6344ccb3dccbb93d5a1df42a9cbfe
nkdkccmbr <nkdkccmbr@users.noreply.github.com>
2019-11-25 15:29:23 +0900

    Add files via upload

--- custom_settings.el
+++ custom_settings.el
@@ -20,20 +20,20 @@
 
 ;; UTF-8環境
 (set-locale-environment nil)
-(prefer-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
-(set-default 'buffer-file-coding-system 'utf-8-with-signature)
-(set-keyboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
 (set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
 
 ;; テーマ
 (load-theme 'manoj-dark t)
 
 ;; フォント
 (add-to-list 'default-frame-alist
-                       '(font . "Ricty Diminished-14"))
+                       '(font . "Ricty Diminished-16"))
 
 ;; スタートアップメッセージを表示させない
 (setq inhibit-startup-message t)
@@ -210,7 +210,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ein flycheck auto-complete))))
+ '(package-selected-packages (quote (flycheck ein auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
